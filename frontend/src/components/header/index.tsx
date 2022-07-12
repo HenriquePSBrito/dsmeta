42f39462a8d474c3d82aeaa60ed55b5f9aaefa90
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-07-12 17:59:32 -0300

    Other components - (Header e SalesCard)

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import iconLogo from '../../assets/img/logo.svg';
+import './styles.css';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={iconLogo} alt="dsmeta" />
+                    <h1>DsMeta</h1>
+                    <p>Desenvolvido por
+                        <a href="https://github.com/HenriquePSBrito"> Henrique PS Brito</a>
+                    </p>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
