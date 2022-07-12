335727dd8c12b974374b321432167ecc1dca339f
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-07-12 07:01:01 -0300

    First Component - Notification Button

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import iconNotification from '../../assets/img/notification-icon.svg';
+import './styles.css';
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={iconNotification} />
+        </div>
+    )
+}
+
+export default NotificationButton
