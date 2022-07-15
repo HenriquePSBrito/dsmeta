f01450b10aa5174a9f92879ea772f4b1352c0d56
HenriquePSBrito <jhowhp@yahoo.com.br>
2022-07-15 17:19:51 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -77,7 +77,7 @@ const SalesCard = () => {
 
                                             <td>
                                                 <div className="dsmeta-red-btn-container">
-                                                    <NotificationButton />
+                                                    <NotificationButton saleId={sale.id}/>
                                                 </div>
                                             </td>
                                         </tr>
